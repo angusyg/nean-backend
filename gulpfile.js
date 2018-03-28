02ad3aed848e1f9dceb5ac62bfb52e059f48c83e
gaelclair <gael.clair@gmail.com>
2018-03-28 11:37:11 +0200

    Modification of gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,31 @@
 const gulp = require('gulp');
 const plugins = require('gulp-load-plugins')();
+const gutil = require('gulp-util');
+const pump = require('pump');
 
 const sourceJs = [
   'src/**/*.js',
   './jsdoc.js',
 ];
+const lintJs = ['src/**/*.js'];
+const pumpPromise = streams => new Promise((resolve, reject) => {
+  pump(streams, (err) => {
+    if (err) {
+      gutil.log(gutil.colors.red('[Error]'), err.toString());
+      reject(err);
+    } else resolve();
+  });
+});
 
+// validates js files
+gulp.task('lint', () => pumpPromise([
+  gulp.src(lintJs),
+  plugins.eslint(),
+  plugins.eslint.format('stylish'),
+  plugins.eslint.failAfterError(),
+]));
+
+// generates JSDoc from js files
 gulp.task('jsdoc', plugins.shell.task('node node_modules/jsdoc/jsdoc.js ' +
   '-d doc ' +
   './README.md ' +
@@ -16,6 +36,5 @@ gulp.task('watch', () => {
   gulp.watch(sourceJs, ['jsdoc']);
 });
 
-// default task (production)
-gulp.task('default', ['copy-js-lib', 'html', 'sass', 'uglify', 'connect', 'watch']);
-gulp.task('postinstall', ['sass', 'uglify']);
+// default task
+gulp.task('default', ['jsdoc', 'watch']);
