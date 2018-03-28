b38e4d75408fc571beff516b35c69362211794e2
gaelclair <gael.clair@gmail.com>
2018-03-28 16:27:15 +0200

    Clean gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,6 @@ const plugins = require('gulp-load-plugins')();
 const gutil = require('gulp-util');
 const pump = require('pump');
 
-const sourceJs = [
-  'src/**/*.js',
-  './jsdoc.js',
-];
 const lintJs = ['src/**/*.js'];
 const pumpPromise = streams => new Promise((resolve, reject) => {
   pump(streams, (err) => {
@@ -25,16 +21,6 @@ gulp.task('lint', () => pumpPromise([
   plugins.eslint.failAfterError(),
 ]));
 
-// generates JSDoc from js files
-gulp.task('jsdoc', plugins.shell.task('node node_modules/jsdoc/jsdoc.js ' +
-  '-d doc ' +
-  './README.md ' +
-  '-r src jsdoc.js'));
-
-// watch files for reload in dev mode
-gulp.task('watch', () => {
-  gulp.watch(sourceJs, ['jsdoc']);
-});
 
 // default task
-gulp.task('default', ['jsdoc', 'watch']);
+gulp.task('default', []);
